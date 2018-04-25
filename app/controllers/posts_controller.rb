b433b3cc6b191e9edb9d6519cbb29c775dd8840a
rufunt <jazzoo@mail.ru>
2018-04-25 21:08:14 +0300

    Add create and show actions for posts and show view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,22 @@ class PostsController < ApplicationController
   end
 
   def new
-    
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
